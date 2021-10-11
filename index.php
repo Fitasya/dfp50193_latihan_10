703d890902c02393de1795714cb2dc4d936e0038
As Syafaa <purpleeisya@gmail.com>
2021-10-11 13:16:42 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+    <style>
+        * {
+            font-size: 30px;
+        }
+        </style>
+</head>
+<body>
+<a href="bmi.php">BMI</a>
+<a href="contoh.php">Contoh</a>
+    
+</body>
+</html>
